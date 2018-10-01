d4d72d42ede1fc29bbc5133282fc96adc67301b1
gillygilstrap <matt.gilstrap0324@gmail.com>
2018-10-01 15:45:51 -0700

    App-5 Project Completed

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Image from './components/ImageFinder'
 
 class App extends Component {
+
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Image 
+        myImage="https://bt-wpstatic.freetls.fastly.net/wp-content/blogs.dir/2904/files/2016/08/st-petersburg.jpg"
+        />
+         
       </div>
     );
   }
---
+++ app-5/src/components/ImageFinder.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+
+const Image = function(props) {
+    return <img src={props.myImage} alt="No Idea"/>
+}
+
+export default Image
