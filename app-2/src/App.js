58beed5396548f5214a0ffcb1a943cd40b121f5b
gillygilstrap <matt.gilstrap0324@gmail.com>
2018-10-01 14:09:28 -0700

    Project Completed

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -3,19 +3,27 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      listItem : ['spaghetti', 'ice cream', 'sushi', 'bologna', 'cheese' ]
+    }
+  }
+
+
   render() {
+    let foodsToDisplay = this.state.listItem.map(function(elem,i) {
+      return (<h2> {elem} </h2>)
+    })
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="App"> 
+      {foodsToDisplay}
       </div>
-    );
+    )
   }
 }
+  
 
 export default App;
