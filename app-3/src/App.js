c417fb2db502c28610875b4ad09a48f83f2f30ae
gillygilstrap <matt.gilstrap0324@gmail.com>
2018-10-01 15:00:24 -0700

    App-3 Project Comleted

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -3,16 +3,36 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      foodList: ['spaghetti', 'ice cream', 'sushi', 'bologna', 'cheese' ],
+      inputBox: ''
+    }
+    this.inputBoxUpdater = this.inputBoxUpdater.bind(this)
+  }
+
+  
+  inputBoxUpdater(e) {
+    this.setState({
+      inputBox: e.target.value
+    })
+  }
+
+
+ 
+
   render() {
+    let tempArray = this.state.foodList.filter(elem => elem.substring(0,this.state.inputBox.length) === this.state.inputBox)
+
+    let displayFoodList = tempArray.map((elem, i) => {
+      return (<h2> {elem} </h2>)
+    })
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <input onChange={this.inputBoxUpdater} value={this.state.inputBox} />
+        { displayFoodList}
       </div>
     );
   }
