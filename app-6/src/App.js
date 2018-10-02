c1ee8d15de7dd7506e6c414dae0835b9e557ecd4
gillygilstrap <matt.gilstrap0324@gmail.com>
2018-10-01 21:23:50 -0700

    App-6 Project Completed

--- app-6/src/App.js
+++ app-6/src/App.js
@@ -1,18 +1,59 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Todo from './components/Todo'
+
 
 class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      taskList: [],
+      task: ''
+    }
+    this.taskListStateUpdater = this.taskListStateUpdater.bind(this)
+    // this.listUpdater = this.listUpdater.bind(this)
+    this.changeHandler = this.changeHandler.bind(this);
+  }
+
+  changeHandler(event){
+    this.setState({
+      task: event.target.value
+    })
+  }
+
+  taskListStateUpdater() {
+    
+    const tempArray = this.state.taskList.slice()
+    tempArray.push(this.state.task)
+    this.setState({
+      taskList: tempArray,
+      task: ''
+    })
+
+    
+    
+  }
+
+ 
+
+
   render() {
+    
+    const taskArray = this.state.taskList.map(item => {
+      return <Todo task={item} />
+    })
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+       <h1>My to-do List:</h1>
+       <input onChange={this.changeHandler} value={this.state.task} type="text"/>
+       <button onClick={this.taskListStateUpdater}>Add</button>
+       <br/>
+       <ul>
+          {taskArray}
+
+        </ul>
       </div>
     );
   }
